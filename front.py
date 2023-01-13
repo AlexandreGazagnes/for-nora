7c6069d9b4568287d74472b277d7bbb316addf69
AlexandreGazagnes <a_syoez@yahoo.fr>
2023-01-13 15:42:06 +0100

    for nora

--- front.py
+++ front.py
@@ -7,6 +7,9 @@ port = 8080
 base = "http://127.0.0.1"
 
 
+option = "None"
+
+
 def get_ids():
     """get ids """
 
@@ -20,6 +23,26 @@ def get_ids():
     return li
 
 
-option = st.selectbox("How would you like to be contacted?", get_ids())
+def predict(id):
+    """predict and id"""
 
+    url = base + ":" + str(port) + "/predict/" + str(id)
+    r = requests.get(url + "")
+    assert int(r.status_code) == 200
+
+    ans = eval(r.text)
+    assert ans in [0, 1, "0", 1]
+
+    return ans
+
+
+# selection
+option = st.selectbox("How would you like to be contacted?", get_ids())
 st.write("You selected:", option)
+
+
+# pred
+if isinstance(option, int):
+    ans = predict(option)
+    ans = "CREDIT ACCORDé" if ans else "CREDIT REFUSé"
+    st.write(f"la réponse est {ans}")
