ca67a228dd98dcabd0438910c40d7549f3b22cc8
AlexandreGazagnes <a_syoez@yahoo.fr>
2023-01-20 15:12:07 +0100

    test front in prod

--- front.py
+++ front.py
@@ -4,47 +4,50 @@ import streamlit as st
 
 import requests
 
-port = 8080
-base = "http://127.0.0.1"
-option = "None"
+# port = 8080
+# base = "http://127.0.0.1"
+# option = "None"
 
 
-def get_ids():
-    """get ids """
+# def get_ids():
+#     """get ids """
 
-    url = base + ":" + str(port) + "/getids"
-    r = requests.get(url + "")
-    assert int(r.status_code) == 200
+#     url = base + ":" + str(port) + "/getids"
+#     r = requests.get(url + "")
+#     assert int(r.status_code) == 200
 
-    li = eval(r.text)
-    assert isinstance(li, list)
+#     li = eval(r.text)
+#     assert isinstance(li, list)
 
-    return li
-
-
-def predict(id):
-    """predict and id"""
-
-    url = base + ":" + str(port) + "/predict/" + str(id)
-    r = requests.get(url + "")
-    assert int(r.status_code) == 200
-
-    ans = eval(r.text)
-    assert ans in [0, 1, "0", "1"]
-
-    return ans
+#     return li
 
 
-# selection
-li_ids = get_ids()
-option = st.selectbox("who is the cleint?", li_ids)
-st.write("You selected:", option)
+# def predict(id):
+#     """predict and id"""
 
-print(option)
+#     url = base + ":" + str(port) + "/predict/" + str(id)
+#     r = requests.get(url + "")
+#     assert int(r.status_code) == 200
+
+#     ans = eval(r.text)
+#     assert ans in [0, 1, "0", "1"]
+
+#     return ans
 
 
-# pred
-if isinstance(option, int):
-    ans = predict(option)
-    ans = "CREDIT ACCORDé" if ans else "CREDIT REFUSé"
-    st.write(f"la réponse est {ans}")
+# # selection
+# li_ids = get_ids()
+# option = st.selectbox("who is the cleint?", li_ids)
+# st.write("You selected:", option)
+
+# print(option)
+
+
+# # pred
+# if isinstance(option, int):
+#     ans = predict(option)
+#     ans = "CREDIT ACCORDé" if ans else "CREDIT REFUSé"
+#     st.write(f"la réponse est {ans}")
+
+
+st.write("hello world")
