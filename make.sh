76d4230f79afebb3823b8c141956e616ae73f52e
AlexandreGazagnes <a_syoez@yahoo.fr>
2023-01-27 17:03:18 +0100

    work on docker

--- make.sh
+++ make.sh
@@ -7,8 +7,8 @@ apt update -y && apt upgrade -y
 apt install htop nano python3-pip python3-venv -y
 
 # env
-python3 -m venv env
-source env/bin/activate
+# python3 -m venv env
+# source env/bin/activate
 
 # pip
 pip install -r requirements.txt
